1933fc3c4e5c1d0eaa747e51ed438412d90cabe3
Froylan  Vargas Gomez <fvargas@MacBook-Pro.local>
2020-07-16 16:03:56 -0500

    Fixing header styles and creating the category list component

---
+++ client/src/components/category-list/category-list.component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CategoryList = () => {
+    return (
+        <div className="category-list">
+            Hola desde Category list. 
+        </div>
+    )
+}
+
+export default CategoryList;
--- client/src/pages/homepage/homepage.component.js
+++ client/src/pages/homepage/homepage.component.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Header from '../../components/header/header.component'
+import CategoryList from '../../components/category-list/category-list.component'
 
 const HomePage = () => {
     return (
         <div className="homepage">
            <Header />
+           <CategoryList />
         </div>
     )
 }
