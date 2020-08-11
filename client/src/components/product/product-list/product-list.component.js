5ac5412e5151a62cacb96abe0ce16128ef47964b
Froylan  Vargas Gomez <fvargas@MacBook-Pro.local>
2020-08-11 12:57:35 -0500

    POC for responsive product list

--- client/src/components/product/product-list/product-list.component.js
+++ client/src/components/product/product-list/product-list.component.js
@@ -5,22 +5,14 @@ import Product from '../product/product.component'
 const ProductList = () => {
     return (
         <div className="product-list">
-            <div className="row">
-                <div className="col-1-of-3">
-                    <div className="product-list__product-box">
-                        <Product />
-                    </div>
-                </div>
-                <div className="col-1-of-3">
-                    <div className="product-list__product-box">
-                        <Product />
-                    </div>
-                </div>
-                <div className="col-1-of-3">
-                    <div className="product-list__product-box">
-                        <Product />
-                    </div>
-                </div>
+            <div className="product-list__product-box">
+                <Product />
+            </div>
+            <div className="product-list__product-box">
+                <Product />
+            </div>
+            <div className="product-list__product-box">
+                <Product />
             </div>
         </div>
     )
